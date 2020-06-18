f22a4a4f0b380f6f6aecd19ddbbf6a03ee246dda
gmul <gmul.choi@gmail.com>
2020-06-19 00:20:40 +0900

    practice factoryMethod - creational pattern

---
+++ src/practice/creational/factoryMethod/App.java
@@ -0,0 +1,26 @@
+package practice.creational.factoryMethod;
+
+import java.util.logging.Logger;
+
+public class App {
+    private final Blacksmith blacksmith;
+
+    public App(Blacksmith blacksmith) {
+        this.blacksmith = blacksmith;
+    }
+
+    public static void main(String[] args) {
+        // Lets go to war with Orc weapons
+        var app = new App(new OrcBlacksmith());
+        app.manufactureWeapons();
+
+        // Lets go to war with Elf weapons
+        app = new App(new ElfBlacksmith());
+        app.manufactureWeapons();
+    }
+
+    private void manufactureWeapons() {
+        var spear = blacksmith.manufactureWeapon(WeaponType.SPEAR);
+        var axe = blacksmith.manufactureWeapon(WeaponType.AXE);
+    }
+}
